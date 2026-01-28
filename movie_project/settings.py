2800c85ae75d76bd6021aaa8a24259641b87e428
Ekta017 <ektapardeshi8@gmail.com>
2026-01-28 13:47:33 +0530

    Add requirements.txt and update settings

--- movie_project/settings.py
+++ movie_project/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -123,3 +124,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ektapardeshi8@gmail.com'
 EMAIL_HOST_PASSWORD = 'dxpr itzs jcas mbqm'
+
+ALLOWED_HOSTS = ['Ekta.pythonanywhere.com']
+DEBUG = False
